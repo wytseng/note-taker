e06e2e9ee8bb841127f618bc1376da3c523d311b
Steph T <wytseng@uci.edu>
2022-12-14 23:23:18 -0800

    change back to index.js and added '/' route

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.use('/api', api);
 
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public','index.html'));
+})
+
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 })
