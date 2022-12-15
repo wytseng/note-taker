c495d11134d190c3c58bdaad9d9a3580eb9dc93d
Steph T <wytseng@uci.edu>
2022-12-15 00:34:00 -0800

    remove unnecessary '/' route

--- server.js
+++ server.js
@@ -6,15 +6,12 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use('/api', api);
 
 app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public','index.html'));
-})
 
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'notes.html'));
