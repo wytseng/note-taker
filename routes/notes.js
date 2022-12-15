960e5e492b2af68337890cceda1af0f829fc76ce
Steph T <wytseng@uci.edu>
2022-12-14 22:42:50 -0800

    adding new note to db works

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,7 @@
 const notes = require('express').Router();
 const fs = require('fs');
 const path = require('path');
+const shortid = require('shortid');
 
 notes.get('/', (req, res) => {
   console.info(`${req.method} request received for notes db`);
@@ -8,6 +9,36 @@ notes.get('/', (req, res) => {
     if (err) throw err;
     res.json(JSON.parse(data));
   })
+});
+
+notes.post('/', (req, res) => {
+
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      id: shortid.generate(),
+    }
+
+    fs.readFile(path.join('db','db.json'), 'utf8', (err, data) => {
+      if (err) throw err;
+      const parsedData = JSON.parse(data);
+      parsedData.push(newNote);
+      fs.writeFile(path.join('db','db.json'), JSON.stringify(parsedData, null, 4), (err) => {
+        err ? console.log(err) : console.log(`\nNew note "${newNote.title}" written db.json`)
+      })
+    })
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    }
+    res.json(response);
+  } else {
+    res.json('Error in posting new notes.')
+  }
 });
 
 
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-// const api = require('./routes');
+const api = require('./routes');
 
 const PORT = process.env.port || 3001;
 
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-// app.use('/api', api);
+app.use('/api', api);
 
 app.use(express.static('public'));
 
